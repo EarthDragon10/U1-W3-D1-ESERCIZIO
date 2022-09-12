9c4af6d46303fd4d6a688dad407063b73f6f9a97
EarthDragon10 <lagudadev@gmail.com>
2022-09-12 15:39:07 +0200

    added some js rules

---
+++ js/naming.js
@@ -0,0 +1,21 @@
+var pet_preferito_1 = "gatto";
+console.log(pet_preferito_1);
+
+var pet = "gatto";
+let pet_dog = "cane";
+console.log(pet, pet - dog);
+
+var petPreferito = "criceto";
+var pet_2 = "coniglio";
+
+document.getElementById("pet").innerHTML =
+	"<p>" + petPreferito + " " + pet_2 + "</p>";
+
+var $pet = "giraffa";
+console.log($pet);
+var _pet = "leone";
+console.log(_pet);
+var _pet2 = "pantera";
+document.write(_pet2);
+
+var num = 12;
