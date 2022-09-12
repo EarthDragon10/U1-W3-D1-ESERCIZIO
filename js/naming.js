9030fb0e0728d0591dc0089baf51762ef7fb9063
EarthDragon10 <lagudadev@gmail.com>
2022-09-12 17:22:00 +0200

    added js rules

--- js/naming.js
+++ js/naming.js
@@ -3,13 +3,13 @@ console.log(pet_preferito_1);
 
 var pet = "gatto";
 let pet_dog = "cane";
-console.log(pet, pet - dog);
+console.log(pet, pet_dog);
 
 var petPreferito = "criceto";
 var pet_2 = "coniglio";
 
 document.getElementById("pet").innerHTML =
-	"<p>" + petPreferito + " " + pet_2 + "</p>";
+	"<p>" + petPreferito + " " + pet_2 + " " + pet + "</p>";
 
 var $pet = "giraffa";
 console.log($pet);
@@ -18,4 +18,9 @@ console.log(_pet);
 var _pet2 = "pantera";
 document.write(_pet2);
 
+let num_person = prompt("Inserisci un numero a caso");
+console.log(parseInt(num_person) + 10);
+
 var num = 12;
+
+document.getElementById("style").style.backgroundColor = "green";
